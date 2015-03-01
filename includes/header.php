a2dfef15a2a0dcb91bfca0cd9a152afcb6c5e24e
HassanAMian <hassanmian83@gmail.com>
2015-03-01 06:30:29 -0500

    changes to navigation

--- includes/header.php
+++ includes/header.php
@@ -16,9 +16,9 @@
 		<div class="wrapper">
 			<h1 class="branding-title"><a href="./"><?php echo $aura; ?></a></h1>
 			<ul class="nav">
-				<li class="shirts <?php if ($section == "shirts") { echo "on"; } ?>"><a href="shirts.php">Shirts</a></li>
-				<li class="shirts <?php if ($section == "pants") { echo "on"; } ?>"><a href="pants.php">Pants</a></li>
-				<li class="contact <?php if ($section == "contact") { echo "on"; } ?>"><a href="contact.php">Contact Us</a></li>
+				<li class="<?php if ($section == "shirts") { echo "on"; } ?>"><a href="shirts.php">Shirts</a></li>
+				<li class="<?php if ($section == "pants") { echo "on"; } ?>"><a href="pants.php">Pants</a></li>
+				<li class="<?php if ($section == "contact") { echo "on"; } ?>"><a href="contact.php">Contact Us</a></li>
 				<li class="cart"><a href="#">Shopping Cart</a></li>
 			</ul>
 		</div>
